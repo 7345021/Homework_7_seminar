235f9ffce94586a38db5052149a05a53cbea2c3c
Andrey <trusov1967@gmail.com>
2022-05-24 08:28:29 +0300

    добавил отображение времени ввода данных

--- var.py
+++ var.py
@@ -1,13 +1,13 @@
+from datetime import datetime
+# import ui
 
-import ui
 
-
-def var(surname, name, num_phone, comment, variant):
+def var( surname, name, num_phone, comment, variant):
     if variant == '1':
         file = open('in log.csv', 'a')
-        file.write(f'{surname}\n{name}\n{num_phone}\n{comment}\n\n')
+        file.write(f'{datetime.today()}\n{surname}\n{name}\n{num_phone}\n{comment}\n\n')
         file.close()
     if variant == '2':
         with open('in log.csv', 'a') as file:
-            file.write(f'{surname};{name};{num_phone};{comment}\n\n')
+            file.write(f'{datetime.today()};{surname};{name};{num_phone};{comment}\n\n')
             file.close()
